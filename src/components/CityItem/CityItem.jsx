a00ef59e85fb9a243f600576e7351a5da2337b1b
AmirZNBZ <amir.na.sufi1376@gmail.com>
2023-10-06 22:44:54 +0330

    added cjties and countries

---
+++ src/components/CityItem/CityItem.jsx
@@ -0,0 +1,28 @@
+import styles from "./CityItem.module.css";
+import propTypes from "prop-types";
+
+const formatDate = (date) =>
+  new Intl.DateTimeFormat("en", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+    weekday: "long",
+  }).format(new Date(date));
+
+function CityItem({ city }) {
+  const { cityName, date, emoji } = city;
+  return (
+    <li className={styles.cityItem}>
+      <span className={styles.emoji}>{emoji}</span>
+      <h3 className={styles.name}>{cityName}</h3>
+      <time className={styles.date}>({formatDate(date)})</time>
+      <button className={styles.deleteBtn}>&times;</button>
+    </li>
+  );
+}
+
+CityItem.propTypes = {
+  city: propTypes.any,
+};
+
+export default CityItem;
--- src/components/CityList/CityList.jsx
+++ src/components/CityList/CityList.jsx
@@ -1,9 +1,28 @@
 import styles from "./CityList.module.css";
 import Spinner from "../Spinner/Spinner";
-
+import PropTypes from "prop-types";
+import CityItem from "../CityItem/CityItem";
+import Message from "../Message/Message";
 function CityList({ cities, isLoading }) {
-  if (isLoading) <Spinner />;
-  return <ul className={styles.cityList}>{cities}</ul>;
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message="add your first city by clicking on a city on the map" />
+    );
+
+  return (
+    <ul className={styles.cityList}>
+      {cities.map((city) => (
+        <CityItem city={city} key={city.id} />
+      ))}
+    </ul>
+  );
 }
+
+CityList.propTypes = {
+  cities: PropTypes.array.isRequired,
+  isLoading: PropTypes.bool.isRequired,
+};
 
 export default CityList;
