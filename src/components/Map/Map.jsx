5cda45c61cbfc842530d55ef3b1076d8fa587b0f
AmirZNBZ <amir.na.sufi1376@gmail.com>
2023-10-09 13:13:03 +0330

    added dynamic routes and redirect with navigation

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
-import styles from "./Map.module.css"
+import { useNavigate, useSearchParams } from "react-router-dom";
+import styles from "./Map.module.css";
+
 function Map() {
+  const navigate = useNavigate();
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
   return (
-    <div className={styles.mapContainer}>Map</div>
-  )
+    <div
+      className={styles.mapContainer}
+      onClick={() => {
+        navigate("form");
+      }}
+    >
+      Map
+      <h1>Map</h1>
+      <h1>
+        position : {lat} , {lng}
+      </h1>
+      <button
+        onClick={() => {
+          setSearchParams({ lat: 33, lng: 49 });
+        }}
+      >
+        Change pos
+      </button>
+    </div>
+  );
 }
 
-export default Map
+export default Map;
