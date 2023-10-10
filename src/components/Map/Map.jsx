c80a3ea462866914c48dd087c2797c05261ef9cc
AmirZNBZ <amir.na.sufi1376@gmail.com>
2023-10-10 13:47:43 +0330

    added context to the app and manage the states

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,32 +1,18 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { useState } from "react";
 
 function Map() {
   const navigate = useNavigate();
-
+  const [mapPosition, setMapPosition] = useState([40, 0]);
   const [searchParams, setSearchParams] = useSearchParams();
 
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
   return (
-    <div
-      className={styles.mapContainer}
-      onClick={() => {
-        navigate("form");
-      }}
-    >
-      Map
-      <h1>Map</h1>
-      <h1>
-        position : {lat} , {lng}
-      </h1>
-      <button
-        onClick={() => {
-          setSearchParams({ lat: 33, lng: 49 });
-        }}
-      >
-        Change pos
-      </button>
+    <div className={styles.mapContainer}>
+
     </div>
   );
 }
